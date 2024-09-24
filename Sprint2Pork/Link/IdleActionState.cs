0dbe979c746de7624b5383a4dca6817304391478
ShashankRaghuraj <shashankraghuraj12@gmail.com>
2024-09-24 12:28:09 -0400

    Got rid of the specs of dirt around sprites

--- Sprint2Pork/Link/IdleActionState.cs
+++ Sprint2Pork/Link/IdleActionState.cs
@@ -14,24 +14,24 @@ namespace Sprint2Pork
         public IdleActionState(Link link)
         {
             this.link = link;
-            Rectangle rect = new Rectangle(69, 11, 16, 16);
+            Rectangle rect = new Rectangle(69, 11, 16, 15);
             bool flipped = false;
             switch (link.directionState)
             {
                 case UpFacingLinkState:
-                    rect = (new Rectangle(100, 0, 16, 16));
+                    rect = (new Rectangle(100, 0, 16, 15));
                     flipped = false;
                     break;
                 case DownFacingLinkState: 
-                    rect = new Rectangle(0, 0, 16, 16);
+                    rect = new Rectangle(0, 0, 16, 15);
                     flipped = false;
                     break;
                 case LeftFacingLinkState: 
-                    rect = new Rectangle(32, 0, 16, 16);
+                    rect = new Rectangle(32, 0, 16, 15);
                     flipped = true;
                     break;
                 case RightFacingLinkState: 
-                    rect = new Rectangle(32, 0, 16, 16);
+                    rect = new Rectangle(32, 0, 16, 15);
                     flipped = false;
                     break;               
             }
