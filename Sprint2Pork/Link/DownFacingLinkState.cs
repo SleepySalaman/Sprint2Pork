89b1b2d5b4623ee00a8402681af99f3b35ad9d4f
ShashankRaghuraj <shashankraghuraj12@gmail.com>
2024-09-24 12:29:24 -0400

    Revert "Got rid of the specs of dirt around sprites"
    This reverts commit 0dbe979c746de7624b5383a4dca6817304391478.

--- Sprint2Pork/Link/DownFacingLinkState.cs
+++ Sprint2Pork/Link/DownFacingLinkState.cs
@@ -16,8 +16,8 @@ namespace Sprint2Pork
         {
             this.link = link;
             List<Rectangle> rects = new List<Rectangle>();
-            rects.Add(new Rectangle(0, 0, 16, 15));
-            rects.Add(new Rectangle(16, 0, 16, 15));
+            rects.Add(new Rectangle(0, 0, 16, 16));
+            rects.Add(new Rectangle(16, 0, 16, 16));
             link.linkSprite = new MovingAnimatedSprite(link.x, link.y, rects, false, 15);
         }
 
--- Sprint2Pork/Link/RightFacingLinkState.cs
+++ Sprint2Pork/Link/RightFacingLinkState.cs
@@ -16,8 +16,8 @@ namespace Sprint2Pork
         {
             this.link = link;
             List<Rectangle> rects = new List<Rectangle>();
-            rects.Add(new Rectangle(32, 0, 16, 15));
-            rects.Add(new Rectangle(50, 0, 16, 15));
+            rects.Add(new Rectangle(32, 0, 16, 16));
+            rects.Add(new Rectangle(50, 0, 16, 16));
             link.linkSprite = new MovingAnimatedSprite(link.x, link.y, rects, false, 15);
         }
 
