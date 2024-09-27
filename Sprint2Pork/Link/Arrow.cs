398e1fe4d0e9cc93ca3bedb0557952b2a972cc9f
prapul-arumilli <prapularumilli@gmail.com>
2024-09-27 14:38:38 -0400

    Updated arrow graphics to start from a sensible position

--- Sprint2Pork/Link/Arrow.cs
+++ Sprint2Pork/Link/Arrow.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Sprint2Pork
@@ -12,6 +13,8 @@ namespace Sprint2Pork
     {
         public int direction = 0;
         Rectangle rect = new Rectangle();
+        int startX = 0;
+        int startY = 0;
         public Arrow(Link link)
         {
             string directionStr = "Down";
@@ -20,25 +23,33 @@ namespace Sprint2Pork
                 case LeftFacingLinkState:
                     direction = 0;
                     directionStr = "Right";
+                    startX = -50;
+                    startY = 50;
                     rect = new Rectangle(154, 16, 5, 16);
                     break;
                 case RightFacingLinkState:
                     direction = 1;
                     directionStr = "Left";
+                    startX = 125;
+                    startY = 30;
                     rect = new Rectangle(154, 16, 5, 16);
                     break;
                 case DownFacingLinkState:
                     direction = 2;
                     directionStr = "Up";
+                    startX = 50;
+                    startY = 133;
                     rect = new Rectangle(154, 16, 5, 16);
                     break;
                 case UpFacingLinkState:
                     direction = 3;
                     directionStr = "Down";
+                    startX = 25;
+                    startY = -601;
                     rect = new Rectangle(154, 16, 5, 16);
                     break;
             }
-            link.linkItemSprite = new MovingNonAnimatedSprite(link.x + link.offsetX, link.y + link.offsetY, rect, directionStr);
+            link.linkItemSprite = new MovingNonAnimatedSprite(link.x + link.offsetX + startX, link.y + link.offsetY + startY, rect, directionStr);
         }
 
         public void Update(Link link)
