a62f4a06c96b26f6e9448e7a9594eaee96589427
ShashankRaghuraj <shashankraghuraj12@gmail.com>
2024-09-28 20:08:06 -0400

    Created Damage State

---
+++ Sprint2Pork/Link/DamagedActionState.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace Sprint2Pork
+{
+    public class DamagedActionState : ILinkActionState
+    {
+        private Link link;
+        private int damageDuration; // Duration for flashing
+        private int frameCounter;
+        private bool isFlashing;
+
+        public DamagedActionState(Link link, int damageDuration)
+        {
+            this.link = link;
+            this.damageDuration = damageDuration;
+            this.frameCounter = 0;
+            this.isFlashing = false;
+            SetFlashingSprite();
+        }
+
+        private void SetFlashingSprite()
+        {
+            List<Rectangle> rects = new List<Rectangle>();
+            bool flipped = false;
+
+            switch (link.directionState)
+            {
+                case LeftFacingLinkState:
+                    if (isFlashing)
+                    {
+                        rects.Add(new Rectangle(34, 129, 16, 16)); // Flash frame
+                        rects.Add(new Rectangle(50, 129, 16, 16));
+                    }
+                    else
+                    {
+                        rects.Add(new Rectangle(34, 0, 16, 15)); // Normal frame
+                        rects.Add(new Rectangle(50, 0, 16, 16));
+                    }
+                    flipped = true;
+                    break;
+                case RightFacingLinkState:
+                    if (isFlashing)
+                    {
+                        rects.Add(new Rectangle(34, 129, 16, 16)); // Flash frame
+                        rects.Add(new Rectangle(50, 129, 16, 16));
+                    }
+                    else
+                    {
+                        rects.Add(new Rectangle(34, 0, 16, 15)); // Normal frame
+                        rects.Add(new Rectangle(50, 0, 16, 16));
+                    }
+                    break;
+                case UpFacingLinkState:
+                    if (isFlashing)
+                    {
+                        rects.Add(new Rectangle(100, 129, 16, 16)); // Flash frame
+                        rects.Add(new Rectangle(116, 129, 16, 16));
+                    }
+                    else
+                    {
+                        rects.Add(new Rectangle(100, 0, 16, 16)); // Normal frame
+                        rects.Add(new Rectangle(116, 0, 16, 16));
+                    }
+                    break;
+                case DownFacingLinkState:
+                    if (isFlashing)
+                    {
+                        rects.Add(new Rectangle(0, 129, 16, 16)); // Flash frame
+                        rects.Add(new Rectangle(16, 129, 16, 16));
+                    }
+                    else
+                    {
+                        rects.Add(new Rectangle(0, 0, 16, 15)); // Normal frame
+                        rects.Add(new Rectangle(16, 0, 16, 15));
+                    }
+                    break;
+            }
+
+            link.linkSprite = new MovingAnimatedSprite(link.x, link.y, rects, flipped, 8, "Damaged");
+        }
+
+        public void BeIdle()
+        {
+            link.actionState = new IdleActionState(link);
+        }
+
+        public void BeMoving()
+        {
+            link.actionState = new MovingActionState(link);
+        }
+
+        public void BeAttacking()
+        {
+            link.actionState = new AttackingActionState(link);
+        }
+
+        public void TakeDamage()
+        {
+            // NO-OP
+        }
+
+        public void Update()
+        {
+            frameCounter++;
+
+            if (frameCounter % 10 == 0)
+            {
+                isFlashing = !isFlashing;
+                SetFlashingSprite();
+            }
+
+            if (frameCounter >= damageDuration)
+            {
+                BeIdle();
+            }
+        }
+    }
+}
