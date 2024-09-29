a90b4e38433309bef59a91f14b9fb03a7cc2ad47
shivamengineer <34991071+shivamengineer@users.noreply.github.com>
2024-09-29 00:45:52 -0400

    stalfos enemy now moves randomly

--- Sprint2Pork/Entity/Moving/Stalfos.cs
+++ Sprint2Pork/Entity/Moving/Stalfos.cs
@@ -7,20 +7,57 @@ using System.Threading.Tasks;
 
 namespace Sprint2Pork.Entity.Moving {
     public class Stalfos : Enemy {
-        
+
+        private int moveX = 0;
+        private int moveY = 0;
+
+        private int moveDistance = 25;
+        private int movedAmount = 0;
+
+        private int direction = 0;
+
+        private bool moving = false;
+
         public Stalfos() {
             sourceRects = new List<Rectangle>() {
                new Rectangle(0, 0, 15, 16),
-               new Rectangle(16, 0, 15, 16)
+               new Rectangle(13, 0, 15, 16)
             };
 
             totalFrames = sourceRects.Count;
+            maxCount = 7;
 
             destinationRect = new Rectangle(initX, initY, rectW / 2, rectH / 2);
         }
 
         public override void Move() {
-
+            if (!moving) {
+                moving = true;
+                direction = new Random().Next(1, 4);
+            } else {
+                movedAmount++;
+                if(movedAmount > moveDistance) {
+                    moving = false;
+                    movedAmount = 0;
+                } else {
+                    switch (direction) {
+                        case 1: //right
+                            moveX++;
+                            break;
+                        case 2: //left
+                            moveX--;
+                            break;
+                        case 3: //up
+                            moveY--;
+                            break;
+                        case 4: //down
+                            moveY++;
+                            break;
+                    }
+                }
+            }
+            destinationRect.X = initX + moveX;
+            destinationRect.Y = initY + moveY;
         }
     }
 }
