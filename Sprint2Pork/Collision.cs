1cbb1774982692cc99f09524f02abd78e0ef1d09
shivamengineer <34991071+shivamengineer@users.noreply.github.com>
2024-10-13 21:53:21 -0400

    Create Collision.cs

---
+++ Sprint2Pork/Collision.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint2Pork {
+    public class Collision {
+
+        public bool collides(Rectangle rect1, Rectangle rect2) {
+            return (rect1.X + rect1.Width > rect2.X &&
+                rect1.X < rect2.X + rect2.Width &&
+                rect1.Y + rect1.Height > rect2.Y &&
+                rect1.Y < rect2.Y + rect2.Height);
+        }
+
+    }
+}
