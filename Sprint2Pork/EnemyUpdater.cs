44a1efa29a73fa967e7690dc45d960af2762c2ba
prapul-arumilli <prapularumilli@gmail.com>
2024-11-20 01:27:07 -0500

    item collision is completed

--- Sprint2Pork/EnemyUpdater.cs
+++ Sprint2Pork/EnemyUpdater.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Sprint2Pork.Blocks;
 using Sprint2Pork.Entity.Moving;
+using System;
 using System.Collections.Generic;
 
 namespace Sprint2Pork
@@ -8,44 +9,73 @@ namespace Sprint2Pork
     public class EnemyUpdater
     {
 
+        // Add a dictionary to track which enemies have been hit during the current activation
+        private static Dictionary<IEnemy, bool> enemyHitTracker = new Dictionary<IEnemy, bool>();
+
         public static void UpdateEnemies(Link link, List<IEnemy> enemies, List<Block> blocks, List<EnemyManager> fireballManagers,
             LinkHealth healthCount)
         {
             if (link.IsLinkUsingItem())
             {
                 link.linkItem.Update(link);
+
+                // Reset the tracker for enemies no longer in range
+                var enemiesOutOfRange = new List<IEnemy>();
+                foreach (var enemy in enemyHitTracker.Keys)
+                {
+                    if (!link.linkItem.Collides(enemy.GetRect()))
+                    {
+                        enemiesOutOfRange.Add(enemy);
+                    }
+                }
+                foreach (var enemy in enemiesOutOfRange)
+                {
+                    enemyHitTracker.Remove(enemy);
+                }
+            }
+            else
+            {
+                // Clear the tracker when Link's item is not in use
+                enemyHitTracker.Clear();
             }
 
             var enemiesToRemove = new List<IEnemy>();
             var fireballsToRemove = new List<EnemyManager>();
-            bool hit = false;
+
             foreach (Enemy enemy in enemies)
             {
                 enemy.Update();
                 enemy.Move(blocks);
-                bool collidesWithLink = Collision.Collides(link.GetRect(), enemy.GetRect());
-                if (collidesWithLink)
+
+                // Check for collision with Link
+                if (Collision.Collides(link.GetRect(), enemy.GetRect()))
                 {
                     link.TakeDamage();
                     healthCount.TakeDamage();
                 }
-                if (link.IsLinkUsingItem())
+
+                // Check if Link's item collides with the enemy
+                if (link.IsLinkUsingItem() && link.linkItem.Collides(enemy.GetRect()))
                 {
-                    if (!hit && link.linkItem.Collides(enemy.GetRect()))
+                    if (!enemyHitTracker.ContainsKey(enemy))
                     {
-                        enemy.TakeDamage(!hit);
-                        hit = true;
+                        enemy.TakeDamage(); // Apply damage only once
+                        enemyHitTracker[enemy] = true; // Mark the enemy as hit
                     }
                 }
+
+                // Check if the enemy should be removed
                 if (enemy.getHealth() <= 0)
                 {
-                    int id = enemy.GetFireballID();
+                    int fireballID = enemy.GetFireballID();
                     enemiesToRemove.Add(enemy);
-                    if (id != 0)
+
+                    // Remove corresponding fireball manager if it exists
+                    if (fireballID != 0)
                     {
                         foreach (var manager in fireballManagers)
                         {
-                            if (manager.GetID() == id)
+                            if (manager.GetID() == fireballID)
                             {
                                 fireballsToRemove.Add(manager);
                             }
@@ -53,15 +83,22 @@ namespace Sprint2Pork
                     }
                 }
             }
+
+            // Remove defeated enemies
             foreach (var enemy in enemiesToRemove)
             {
                 enemies.Remove(enemy);
             }
+
+            // Remove fireball managers associated with defeated enemies
             foreach (var manager in fireballsToRemove)
             {
                 fireballManagers.Remove(manager);
             }
         }
+
+
+
 
         public static void UpdateFireballs(EnemyManager enemyManager, ref Link link, ref List<EnemyManager> fireballManagers,
             GameTime gameTime, ref LinkHealth health)
