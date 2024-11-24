b24731a57854d39640c2b92cea95601c5e3a1150
ShashankRaghuraj <80557791+ShashankRaghuraj@users.noreply.github.com>
2024-11-24 18:47:00 -0500

    Refactored Block Collision through it's own class

--- Sprint2Pork/Game1.cs
+++ Sprint2Pork/Game1.cs
@@ -272,63 +272,7 @@ namespace Sprint2Pork
             link.UpdateInvincibilityTimer(gameTime);
             link.actionState.Update();
             link.LinkSpriteUpdate();
-            HandleBlockCollision(linkPreviousX, linkPreviousY);
-        }
-        private void HandleBlockCollision(int linkPreviousX, int linkPreviousY)
-        {
-            foreach (Block block in blocks)
-            {
-                if (Collision.Collides(link.GetRect(), block.getBoundingBox())){
-                    if (block.IsMovable){
-
-                        Vector2 potentialPosition = block.Position;
-                        bool canMove = true;
-
-                        if (link.directionState is LeftFacingLinkState)
-                            potentialPosition = new Vector2(block.Position.X - Block.TileSize, block.Position.Y);
-                        else if (link.directionState is RightFacingLinkState)
-                            potentialPosition = new Vector2(block.Position.X + Block.TileSize, block.Position.Y);
-                        else if (link.directionState is UpFacingLinkState)
-                            potentialPosition = new Vector2(block.Position.X, block.Position.Y - Block.TileSize);
-                        else if (link.directionState is DownFacingLinkState)
-                            potentialPosition = new Vector2(block.Position.X, block.Position.Y + Block.TileSize);
-
-                        foreach (Block otherBlock in blocks){
-                            if (otherBlock != block && Collision.Collides(new Rectangle((int)potentialPosition.X, (int)potentialPosition.Y, block.BoundingBox.Width, block.BoundingBox.Height), otherBlock.getBoundingBox())){
-                                canMove = false;
-                                break;
-                            }
-                        }
-
-                        if (canMove)
-                        {
-                            block.Move(link.directionState);
-                        }
-                        else
-                        {
-                            link.SetX(linkPreviousX);
-                            link.SetY(linkPreviousY);
-                        }
-                    }
-                    else
-                    {
-                        link.SetX(linkPreviousX);
-                        link.SetY(linkPreviousY);
-                    }
-                    break;
-                }
-                if (link.linkItem.Collides(block.getBoundingBox()))
-                {
-                    link.StopLinkItem();
-                }
-            }
-            if (link.IsLinkUsingItem())
-            {
-                if (Collision.CollidesWithOutside(link.linkItem.SpriteGet().GetRect(), roomBoundingBox))
-                {
-                    link.StopLinkItem();
-                }
-            }
+            BlockCollisionHandler.HandleBlockCollision(link, linkPreviousX, linkPreviousY, blocks, roomBoundingBox);
         }
 
         private void UpdateGroundItems()
