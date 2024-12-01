eb6b8a86347a5a8e2ad4ac8e464e998cb537b158
shivamengineer <34991071+shivamengineer@users.noreply.github.com>
2024-12-01 15:40:15 -0500

    added background and cannon

--- Sprint2Pork/Popups/ClashOfClans.cs
+++ Sprint2Pork/Popups/ClashOfClans.cs
@@ -10,7 +10,7 @@ namespace Sprint2Pork.Popups {
         private Popup gamePopup;
 
         public ClashOfClans() {
-            gamePopup = new Popup(100, 100, 1200, 600);
+            gamePopup = new Popup(100, 100, 1200, 580);
             gamePopup.AddImage("../th1.png", 0, 0, 200, 200);
             for(int i = 6; i >= 0; i--) {
                 gamePopup.AddImage("../Wall1.png", 220, 30 * i - 4, 20, 30);
@@ -18,6 +18,9 @@ namespace Sprint2Pork.Popups {
             for(int i = 0; i < 12; i++) {
                 gamePopup.AddImage("../Wall1.png", 20 * i, 200, 20, 40);
             }
+            gamePopup.AddImage("../Cannon1.png", 240, 0, 80, 80);
+
+            gamePopup.AddImage("../Grass.jpg", 0, -340, 1200, 1200);
         }
 
         public void Update() {
