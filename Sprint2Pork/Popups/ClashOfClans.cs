1f3ced65d86919f605c228298635e8d892e4e430
shivamengineer <34991071+shivamengineer@users.noreply.github.com>
2024-12-01 15:18:20 -0500

    created walls

--- Sprint2Pork/Popups/ClashOfClans.cs
+++ Sprint2Pork/Popups/ClashOfClans.cs
@@ -10,8 +10,14 @@ namespace Sprint2Pork.Popups {
         private Popup gamePopup;
 
         public ClashOfClans() {
-            gamePopup = new Popup(300, 300, 600, 600);
+            gamePopup = new Popup(100, 100, 1200, 600);
             gamePopup.AddImage("../th1.png", 0, 0, 200, 200);
+            for(int i = 6; i >= 0; i--) {
+                gamePopup.AddImage("../Wall1.png", 220, 30 * i - 4, 20, 30);
+            }
+            for(int i = 0; i < 12; i++) {
+                gamePopup.AddImage("../Wall1.png", 20 * i, 200, 20, 40);
+            }
         }
 
         public void Update() {
